952135cd958bb39729e0a350e1989f27a8f536c3
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-07 11:07:36 -0600

    Added header.php initial setup using wp_head() and bloginfo()

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo( 'title' ); ?></title>
+    <?php
+    // This outputs any scripts, styles, metadata, that our plugin/theme need.
+    wp_head();
+    ?>
+</head>
+<body>
+ <h1><?php bloginfo( '' ); ?></h1>    
