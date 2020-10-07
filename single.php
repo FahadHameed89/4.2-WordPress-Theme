22fdbcf95c707dcd75f346131702dedd2adc45fa
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-07 12:00:09 -0600

    Added single.php Category list link

--- single.php
+++ single.php
@@ -11,6 +11,28 @@ Blog Post (Single):
                  <?php echo get_the_date(); // WP function, gets the date of post ?>
             </time>
     </p>
+    <h3>Post Categories</h3>
+    <ul>
+        <?php
+        // Get an array of category ID Numbers for THIS post. 
+        $categories = wp_get_post_categories( get_the_id() );
+        // Loop through the post categories
+        foreach ( $categories as $catID )
+        {   // Retrieve the WP_Term object for this ID. 
+            $category = get_category( $catID );
+            // Retrieve the current category's URL
+            $categoryURL = get_term_link( $category);
+            // Output the formatted List item (LI)
+            ?>
+            <li>
+                <a href="<?php echo $categoryURL;?>">
+                <?php echo $category->name; // Property from WP_Term?>
+                </a>
+            </li>
+            <?php 
+        }
+        ?>
+    </ul>
     <h3>Post Content</h3>
     <?php the_content();    // Displays post content    ?>
 </article>
